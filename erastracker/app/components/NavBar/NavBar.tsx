a706f5c881fa2bca1b71da5c9ea040430238e988
Hien Giang <gianghien2999@gmail.com>
2024-08-30 14:17:30 +0200

    import logo in nav

--- erastracker/app/components/NavBar/NavBar.tsx
+++ erastracker/app/components/NavBar/NavBar.tsx
@@ -1,44 +1,60 @@
 import React from 'react'
+import { FaHome } from "react-icons/fa";
+import erasLogo from '../../../public/img/erasLogo.jpg';
+import Image from 'next/image';
 
 const NavBar = () => {
   return (
-    <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-            {/* Navbar */}
-            <div className="navbar bg-base-300 w-full">
-            <div className="flex-none lg:hidden">
-                <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+    <div className="navbar bg-base-100">
+        <div className="navbar-start">
+            <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <FaHome className="size-6" />
+            </div>
+            <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li><a>Homepage</a></li>
+                <li><a>Portfolio</a></li>
+                <li><a>About</a></li>
+            </ul>
+            </div>
+        </div>
+        <div className="navbar-center">
+            <Image src={erasLogo} alt={''}/>
+        </div>
+        <div className="navbar-end">
+            <button className="btn btn-ghost btn-circle">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            </button>
+            <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block h-6 w-6 stroke-current">
-                    <path
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"></path>
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                 </svg>
-                </label>
+                <span className="badge badge-xs badge-primary indicator-item"></span>
             </div>
-            <div className="mx-2 flex-1 px-2">Navbar Title</div>
-            <div className="hidden flex-none lg:block">
-                <ul className="menu menu-horizontal">
-                {/* Navbar menu content here */}
-                <li><a>Navbar Item 1</a></li>
-                <li><a>Navbar Item 2</a></li>
-                </ul>
-            </div>
-            </div>
-        </div>
-        <div className="drawer-side">
-            <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-            <ul className="menu bg-base-200 min-h-full w-80 p-4">
-            {/* Sidebar content here */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-            </ul>
+            </button>
         </div>
         </div>
   )
