9b52a002c55c2c0a5081f112159cc75ddcccffb2
Hien Giang <gianghien2999@gmail.com>
2024-08-30 14:17:49 +0200

    design change

--- erastracker/app/components/ProductCard/ProductCard.tsx
+++ erastracker/app/components/ProductCard/ProductCard.tsx
@@ -4,7 +4,7 @@ import AddToCart from '../AddToCart';
 
 const ProductCard = () => {
   return (
-    <div className='p-5 my-5 bg-sky-200 text-white text-xl hover:bg-sky-100'>
+    <div className='p-5 my-5 hover:bg-sky-100'>
         <AddToCart />
     </div>
   )
