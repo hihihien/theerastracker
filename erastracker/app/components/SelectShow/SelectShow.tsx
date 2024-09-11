59d9c286ec8fd22fdffc0dc0ea0d5860b2114b75
Hien Giang <gianghien2999@gmail.com>
2024-09-12 00:20:45 +0200

    add more info

--- erastracker/app/components/SelectShow/SelectShow.tsx
+++ erastracker/app/components/SelectShow/SelectShow.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 import speaknow from '../../../public/img/speaknow.jpg'
 
 interface SurpriseSongs {
-    acoustic: string;
-    piano: string;
+    acoustic: string[];
+    piano: string[];
 }
 
 interface Costumes {
@@ -18,10 +18,12 @@ interface Costumes {
 interface Show {
     date: string;
     city: string;
+    state: string;
+    opening: string;
     country: string;
     surpriseSongs: SurpriseSongs;
-    guest: string;
-    costumes: Costumes;
+    guest?: string;  // Optional
+    costumes?: Costumes;  // Optional
 }
 
 const SelectShow: React.FC = () => {
@@ -29,23 +31,22 @@ const SelectShow: React.FC = () => {
     const [selectedShow, setSelectedShow] = useState<Show | null>(null);
 
     useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const response = await fetch("/metadata.json");
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/metadata.json");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.json();
+                console.log("Fetched Data:", data); // Log the fetched data
+                setShows(data.shows);
+            } catch (error) {
+                console.error("Fetch error:", error); // Log any fetch errors
             }
-            const data = await response.json();
-            console.log("Fetched Data:", data); // Log the fetched data
-            setShows(data.shows);
-        } catch (error) {
-            console.error("Fetch error:", error); // Log any fetch errors
-        }
-    };
+        };
 
-    fetchData();
-}, []);
-
+        fetchData();
+    }, []);
 
     const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const selectedDate = event.target.value;
@@ -54,52 +55,55 @@ const SelectShow: React.FC = () => {
     };
 
     return (
-    <div className='artboard artboard-horizontal'>
-        <div className="flex w-full flex-col items-center justify-center gap-4 place-items-stretch">
-            <div className="prose p-6"><h2>Select Your Show</h2></div>
-            
-            <select
-                className="select select-primary w-full max-w-xs"
-                onChange={handleSelectChange}
-                value={selectedShow ? selectedShow.date : ""}
-            >
-                <option disabled value="">
-                    Choose your show
-                </option>
-                {shows.length === 0 ? (
-                    <option disabled>Loading shows...</option>
-                ) : (
-                    shows.map((show, index) => (
-                        <option key={index} value={show.date}>
-                            {`${show.date} - ${show.city}, ${show.country}`}
-                        </option>
-                    ))
-                )}
-            </select>
-            {selectedShow && (
-                    <div className="card card-side size-140 border-2 border-inherit shadow-xl mt-4 prose-img:rounded-l-lg">
-                        <figure>
-                            <Image
-                            src={speaknow}
-                            alt="surprise song img" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="text-lg font-bold mb-2">⭐ Show Details ⭐</h2>
-                            <p><strong>Surprise Songs:</strong></p>
-                            <p>🎸 Acoustic: {selectedShow.surpriseSongs.acoustic}</p>
-                            <p>🎹 Piano: {selectedShow.surpriseSongs.piano}</p>
-                            <p className='mt-2'><strong>Guest:</strong> {selectedShow.guest}</p>
-                            <p className='mt-2'><strong>Watch:</strong></p>
+        <div className='artboard artboard-horizontal'>
+            <div className="flex w-full flex-col items-center justify-center gap-4 place-items-stretch">
+                <div className="prose p-6"><h2>Select Your Show</h2></div>
+                
+                <select
+                    className="select select-primary w-full max-w-xs"
+                    onChange={handleSelectChange}
+                    value={selectedShow ? selectedShow.date : ""}
+                >
+                    <option disabled value="">
+                        Choose your show
+                    </option>
+                    {shows.length === 0 ? (
+                        <option disabled>Loading shows...</option>
+                    ) : (
+                        shows.map((show, index) => (
+                            <option key={index} value={show.date}>
+                                {`${show.date} - ${show.city}, ${show.country}`}
+                            </option>
+                        ))
+                    )}
+                </select>
+                {selectedShow && (
+                    <div className="flex w-full flex-col lg:flex-row">
+                        <div className="card card-side size-400 rounded-box border-2 border-inherit shadow-xl m-4">
+                            <figure>
+                                <Image
+                                src={speaknow}
+                                alt="surprise song img" />
+                            </figure>
+                            <div className="card-body antialiased md:subpixel-antialiased">
+                                <div>
+                                    <h2 className="text-lg font-bold mb-3">⭐ Show Details ⭐</h2>
+                                    <p className='mt-2 oldstyle-nums'>📆&nbsp;&nbsp;{selectedShow.date}</p>
+                                    <p className='mt-2'>📍&nbsp;&nbsp;{selectedShow.city}{selectedShow.state ? `, ${selectedShow.state}` : ""}, {selectedShow.country}</p>
+                                    <p className='mt-2'><strong>Surprise Songs:</strong></p>
+                                    <p className='mt-1 italic'>🎸 {selectedShow.surpriseSongs.acoustic.join(", ")}</p>
+                                    <p className='mt-1 italic'>🎹 {selectedShow.surpriseSongs.piano.join(", ")}</p>
+                                    <p className='mt-2'><strong>Opening Artist:</strong> {selectedShow.opening || "No guest"}</p>
+                                    <p className='mt-2'><strong>Special Guest:</strong> {selectedShow.guest || "No guest"}</p>
+                                    <p className='mt-2'><strong>Watch:</strong></p>
+                                </div>
+                            </div>
                         </div>
-                        
                     </div>
-                    
-                    
                 )}
+            </div>
         </div>
-    </div>
-);
-
+    );
 };
 
 export default SelectShow;
