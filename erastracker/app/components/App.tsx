2d62b95399f552a7e5fea7336b02bb41aff3daa4
Hien <74279944+hihihien@users.noreply.github.com>
2024-11-09 22:26:55 +0100

    add component to share state

---
+++ erastracker/app/components/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Hero from './Hero/Hero';
+import SongsTabs from './SongsTabs';
+
+// Album names corresponding to the themes and images
+const albums = [
+    "Taylor Swift", "Fearless", "Speak Now", "Red", "1989", 
+    "Reputation", "Lover", "Folklore", "Evermore", "Midnights", "The Tortured Poets Department"
+];
+
+const themes = [
+    'lemonade', 'cyberpunk', 'synthwave', 'red', 'pastel',
+    'black', 'valentine', 'luxury', 'retro', 'aqua', 'wireframe'
+];
+
+const App: React.FC = () => {
+    const [selectedAlbum, setSelectedAlbum] = useState<string>(albums[albums.length - 1]);
+    const [theme, setTheme] = useState<string>(themes[themes.length - 1]);
+
+    const handleAlbumClick = (index: number) => {
+        setSelectedAlbum(albums[index]);
+        setTheme(themes[index]);
+        document.documentElement.setAttribute('data-theme', themes[index]);
+    };
+
+    return (
+        <div>
+            <Hero onAlbumClick={handleAlbumClick} selectedTheme={theme} />
+            <SongsTabs selectedAlbum={selectedAlbum} />
+        </div>
+    );
+};
+
+export default App;
